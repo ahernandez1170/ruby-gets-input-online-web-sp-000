c197dc0972036086931e33b242493d8af0ea28e1
Austin Hernandez <ahernandez1170@gmail.com>
2020-10-27 02:13:02 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,10 +1,6 @@
 
 def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
-<<<<<<< HEAD
 gets(name)
-=======
-gets (name)
->>>>>>> 5634361df3354c804b2128e6958cd950fc24383e
-end 
+end
 
